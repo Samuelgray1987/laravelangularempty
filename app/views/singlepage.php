ac5c08679c9f1dd06bf83df1388f03a296a242d7
Samuel Gray <samuelgray1987@gmail.com>
2013-12-16 22:29:58 +0000

    Added html temp

---
+++ app/views/singlepage.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en" ng-app="app">
+<head>
+  <meta charset="UTF-8">
+  <title>Empty Angular Laravel</title>
+  <link rel="stylesheet" href="/css/normalize.css">
+  <link rel="stylesheet" href="/css/foundation.min.css">
+  <link rel="stylesheet" href="/css/style.css">
+  <script src="/js/angular.js"></script>
+  <script src="/js/angular-sanitize.js"></script>
+  <script src="/js/underscore.js"></script>
+  <script src="/js/app.js"></script>
+</head>
+<body>
+
+  <div class="row">
+    <div class="large-12">
+      <h1>Login With Angular JS</h1>
+      <div class="row">
+        <div class="large-6 large-offset-3">
+          <div id="flash" class="alert-box alert" ng-show="flash">
+            {{ flash }}
+          </div>
+        </div>
+      </div>
+      <div id="view" ng-view></div>
+    </div>
+  </div>
+
+</body>
+</html>
